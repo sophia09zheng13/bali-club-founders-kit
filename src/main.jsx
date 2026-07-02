af19537eb6c07eb8b59631d84dce579135c21ea5
sophia09zheng13 <sophia09zheng13@gmail.com>
2026-07-02 10:56:26 +0800

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import FoundersKit from "./FoundersKit.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FoundersKit />
+    <Analytics />
   </React.StrictMode>
 );
